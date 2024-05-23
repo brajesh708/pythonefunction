c731b903f3ba939f345ce59716f3f5fac2306d1e
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-05-23 14:27:37 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,57 @@
+
+# x=1
+# while x<=10:
+#     # print(x)
+#     print (x ,end=",")
+# x=x+1
+
+
+# n=int(input("enter any number"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+
+n=int(input("enter range :"))
+i=1
+while 1<=n:
+    if(i%2==0):
+        if i<=(n-1):
+            print(i,end=",")
+        else:
+            print(i)
+            i=i+1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    

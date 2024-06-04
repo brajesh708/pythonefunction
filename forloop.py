f2bc85db09629841495c5be0043038f1f3b5ba10
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-06-04 16:22:59 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,64 @@
+
+
+
+# range (start ,end , step)
+# range(end)
+
+# x=range(5)
+# print(x)
+# print(list(x))
+# print(tuple(x))
+# print(set(x))
+
+
+# x=(range(2,50,2))
+# print(x)
+# print(list(x))
+# print(tuple(x))
+
+
+# x=(range(2,50,-2))
+# print(x)
+# print(list(x))
+
+
+# x=(range(2,22,2))
+# print(list(x))
+
+
+# for loop sentext 
+# for i in range(1,11):
+#     print(i)
+#     # print("hello")
+#     # print("welcome")
+#     print(i ,end=",")
+    
+    
+# for x in range (2,20,2):
+#         print(x ,end=",")
+
+
+# for x in range (1,20,1):
+#         print(x ,end=",")
+        
+#  even number print kare
+# for x in range (2,20):
+#     if(x%2==0):
+#      print(x ,end=",")
+
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#   for y in fruits:
+#     print(x, y,end=",")
+
+
+   
+my_list=[10,20,20,202]  
+my_frozenset=frozenset(my_list)  
+print(my_frozenset)  
+print(type(my_frozenset)) 
+        
+        
+
